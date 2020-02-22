8a3e04eb2e46dd37eecd9755cee2d6bac4642248
pablokmieleski <pablo.kmieleski@gmail.com>
2020-02-22 19:59:17 +0000

    feat.js upgrade

--- src/js/main.js
+++ src/js/main.js
@@ -20,17 +20,13 @@ if ('serviceWorker' in navigator) {
 
 const load = document.querySelector('.form__button--load-js');
 const save = document.querySelector('.form__button--save-js');
-
-
-
-
-
+const message = document.querySelector('.form__message--js');
 
 save.addEventListener('click', (e) => {
   e.preventDefault();
   localStorage.setItem('message', message.value);
 })
-const message = document.querySelector('.form__message--js');
+
 load.addEventListener('click', (e) => {
   e.preventDefault();
   message.value = localStorage.getItem('message');
